2b503d4944d2ac0c1606d5f88efcfd4acf838230
Lysionak Denis <dzianislysionak@gamil.com>
2024-08-27 10:30:40 +0400

    Add unit tests

---
+++ src/users/auth.service.spec.ts
@@ -0,0 +1,65 @@
+import { Test } from '@nestjs/testing';
+import { AuthService } from './auth.service';
+import { UsersService } from './users.service';
+import { User } from './user.entity';
+import { BadRequestException, NotFoundException } from '@nestjs/common';
+
+describe('AuthService', () => {
+  let service: AuthService;
+  let fakeUserService: Partial<UsersService>;
+
+  beforeEach(async () => {
+    fakeUserService = {
+      find: () => Promise.resolve([]),
+      create: (email: string, password: string) =>
+        Promise.resolve({ id: 1, email, password } as User),
+    };
+    const module = await Test.createTestingModule({
+      providers: [
+        AuthService,
+        { provide: UsersService, useValue: fakeUserService },
+      ],
+    }).compile();
+
+    service = module.get(AuthService);
+  });
+
+  it('can create  an instance of auth service', async () => {
+    expect(service).toBeDefined();
+  });
+
+  it('Create a user with a salted and hashed password', async () => {
+    const user = await service.signUp('asdasds@asd.sd', 'test_123');
+
+    expect(user.password).not.toEqual('test_123');
+    const [salt, hash] = user.password.split('.');
+
+    expect(salt).toBeDefined();
+    expect(hash).toBeDefined();
+  });
+
+  it('throws an error if user signs up with email that already in use', async () => {
+    fakeUserService.find = () =>
+      Promise.resolve([{ id: 1, email: 'a', password: 'a' } as User]);
+
+    await expect(service.signUp('asdf@asdf.com', 'asdf')).rejects.toThrow(
+      BadRequestException,
+    );
+  });
+
+  it('throws if signin is called with an unused email', async () => {
+    await expect(
+      service.signIn('asdflkj@asdlfkj.com', 'passdflkj'),
+    ).rejects.toThrow(NotFoundException);
+  });
+
+  it('throws if an invalid password is provided', async () => {
+    fakeUserService.find = () =>
+      Promise.resolve([
+        { email: 'asdf@asdf.com', password: 'laskdjf' } as User,
+      ]);
+    await expect(
+      service.signIn('laskdjf@alskdfj.com', 'passowrd'),
+    ).rejects.toThrow(BadRequestException);
+  });
+});
